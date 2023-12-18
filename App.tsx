4944ce25416bbff61ebce7a486060969b88ee7d9
mac <mac@macs-Mac-mini.local>
2023-12-18 10:33:57 +0600

    alerm at certain time wokirng

--- App.tsx
+++ App.tsx
@@ -5,13 +5,14 @@ import TrackPlayer, { Track } from 'react-native-track-player';
 function MusicPlayer() {
   useEffect(() => {
     setupPlayer();
+    schedulePlay();
   }, []);
 
   const setupPlayer = async () => {
     await TrackPlayer.setupPlayer();
     const tracks: Track[] = [
       {
-        id: 1, // Use string IDs instead of numbers
+        id: 1,
         url: 'https://file-examples.com/storage/fe6e60472c657f757a0e725/2017/11/file_example_MP3_700KB.mp3',
         title: 'Track 1',
         artist: 'Artist 1',
@@ -27,12 +28,27 @@ function MusicPlayer() {
   };
 
   const playTrack = async () => {
-    await TrackPlayer.skip(1); // Use the string ID here
+    await TrackPlayer.skip(1);
     await TrackPlayer.play();
   };
 
   const pauseTrack = async () => {
     await TrackPlayer.pause();
+  };
+
+  const schedulePlay = () => {
+    const now = new Date();
+    const targetTime = new Date();
+
+    targetTime.setHours(10, 33, 0, 0); // Set target time to 10:25 AM
+
+    // If the time has already passed today, set it for tomorrow
+    if (now > targetTime) {
+      targetTime.setDate(targetTime.getDate() + 1);
+    }
+
+    const timeout = targetTime.getTime() - now.getTime();
+    setTimeout(playTrack, timeout);
   };
 
   return (
